49ba68824d9827cd69e11b3c76024a936749a68f
Jarreth <jarrethfleischer@gmail.com>
2023-05-29 12:13:48 +0800

    first commit

---
+++ Dryrun/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbUsername = New System.Windows.Forms.TextBox()
+        Me.tbPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(169, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(206, 75)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Login"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Username:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 184)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 28)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Password:"
+        '
+        'tbUsername
+        '
+        Me.tbUsername.Location = New System.Drawing.Point(168, 126)
+        Me.tbUsername.Name = "tbUsername"
+        Me.tbUsername.Size = New System.Drawing.Size(360, 20)
+        Me.tbUsername.TabIndex = 3
+        '
+        'tbPassword
+        '
+        Me.tbPassword.Location = New System.Drawing.Point(168, 192)
+        Me.tbPassword.Name = "tbPassword"
+        Me.tbPassword.Size = New System.Drawing.Size(360, 20)
+        Me.tbPassword.TabIndex = 4
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.ForestGreen
+        Me.btnLogin.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnLogin.Location = New System.Drawing.Point(439, 248)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(89, 42)
+        Me.btnLogin.TabIndex = 5
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Red
+        Me.btnReset.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnReset.Location = New System.Drawing.Point(330, 248)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(89, 42)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(551, 302)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.tbPassword)
+        Me.Controls.Add(Me.tbUsername)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbUsername As TextBox
+    Friend WithEvents tbPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ Dryrun/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Controls.Clear()
+        InitializeComponent()
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim connectionString As String = "Data Source=Z4P5-062; Initial Catalog = login;Trusted_Connection = True;"
+        Dim connection As New SqlConnection(connectionString)
+        connection.Open()
+
+        Dim query As String = "SELECT * FROM login WHERE username = @username AND password = @password"
+        Dim command As New SqlCommand(query, connection)
+
+        command.Parameters.AddWithValue("@username", tbUsername.Text)
+        command.Parameters.AddWithValue("@password", tbPassword.Text)
+
+        Dim reader As SqlDataReader = command.ExecuteReader()
+
+        If reader.HasRows Then
+            While reader.Read()
+                Dim username As String = reader("username").ToString
+                Dim password As String = reader("password").ToString
+            End While
+            MsgBox("Welcome!")
+        Else
+            MsgBox("Unidentified User!")
+        End If
+    End Sub
+End Class
